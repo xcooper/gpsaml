4be2eaa024b2d24af7fa697ee46991518213ff3b
Cooper <xcooper1030@gmail.com>
2023-05-23 23:47:56 +0800

    handle openconnect for Windows.

--- src/openconnect.js
+++ src/openconnect.js
@@ -1,5 +1,7 @@
 const {userAgent} = require("./consts");
 const {URLSearchParams} = require("url");
+const {exec} = require('child_process');
+const log = require("loglevel");
 
 function connectVpn(
   loginResp,
@@ -7,9 +9,17 @@ function connectVpn(
   fingerprint,
   hostname
 ) {
-  var params = new URLSearchParams(loginResp);
-  // console.log(`echo '${params.toString()}' | sudo openconnect --dump-http-traffic --protocol=gp -u ${loginResp.domain}\\${samlUsername} --os=linux --version-string="Ubuntu Linux" --useragent="${userAgent}" --cookie-on-stdin --servercert ${fingerprint} ${hostname}`);
-  console.log(`sudo openconnect --dump-http-traffic --protocol=gp -u ${loginResp.domain}\\${samlUsername} --os=linux --version-string="Ubuntu Linux" --useragent="${userAgent}" --cookie='${params.toString()}' --servercert ${fingerprint} ${hostname}`);
+  let cookie = new URLSearchParams(loginResp);
+  const proc = exec(
+    // should handle differently on different OS, this is on Windows.
+    `sudo --new openconnect --dump-http-traffic --protocol=gp -u ${loginResp.domain}\\${samlUsername} --os=linux --version-string="Ubuntu Linux" --useragent="${userAgent}" --cookie="${cookie.toString()}" --servercert ${fingerprint} ${hostname}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        throw error;
+      }
+      console.log('stdout: ' + stdout);
+    }
+  );
 }
 
 module.exports = {connectVpn};
